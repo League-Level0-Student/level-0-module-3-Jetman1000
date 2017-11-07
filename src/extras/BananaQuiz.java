2252a614982039278493024960ff5ca2e7ab3e7e
Jetman1000 <31870437+Jetman1000@users.noreply.github.com>
2017-11-06 17:53:01 -0800

    Add files via upload
    DizzyRobot, BananaQuiz

--- src/extras/BananaQuiz.java
+++ src/extras/BananaQuiz.java
@@ -10,15 +10,37 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String cool= "no";
+		String nice= "yes";
+		String y=JOptionPane.showInputDialog("Do you like bananas?");
 		//2. if they say no, 
+		if(y==cool){
+			JOptionPane.showMessageDialog(null, "Boi you Crazy!");
+			System.exit(0);
+		}
+		
 			//tell them they are crazy 
+		
 			//and end quiz
+		
 		//3. if they say yes
+		if(y==nice){
+			String v=JOptionPane.showInputDialog("what's your favorite hobby");
+			JOptionPane.showMessageDialog(null, v+" is much with bananas! ");
+		}
 		//	ask them what is their favorite hobby
+		
 		//	show a pop up that says "<your hobby> is much better with bananas!"
+		
 
-		//4. OPTIONAL: if they say something other than “yes” or “no”
-		//	show a pop up that says “You are bananas!”
+		//4. OPTIONAL: if they say something other than â€œyesâ€� or â€œnoâ€�
+		if(y != nice){
+			JOptionPane.showMessageDialog(null, "You're bananas!");
+		}
+		if(y != cool){
+			JOptionPane.showMessageDialog(null, "You're bananas!");
+		}
+		//	show a pop up that says â€œYou are bananas!â€�
 	
 	}
 
--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,6 +3,8 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
@@ -10,15 +12,17 @@ public class DizzyRobot {
 	public static void main(String[] args) {
 		
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+String q=JOptionPane.showInputDialog("from 1-10, how dizzy is your robot");
  // 1. Use the dance method to make the robot spin.
-	
-
+	int x=Integer.parseInt(q);
+dance(x);
 	}
 
 	static void dance(int numberOfSpins) {
 		for (int i = 0; i < numberOfSpins; i++) {
+			robot.setSpeed(90);
 			robot.turn(360);
+			
 		}
 	}
 }
